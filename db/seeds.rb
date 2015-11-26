455c4b2faaf5e2d2b4149414f588ee1187bf26fe
shamkeev <shamkeyev@mail.ru>
2015-11-27 01:00:53 +0600

    Added some more user to seeds. Changed email to safe_email to work with russian locale

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,24 +18,24 @@ password = 'password'
 
 10.times do
   User.create!(
-      name: Faker::Name.name,
+      name: Faker::Name.first_name,
       surname: Faker::Name.last_name,
       middlename: Faker::Name.first_name,
-      gender: ['male', 'female'].sample,
+      gender: ['Мужчина', 'Женщина'].sample,
       birthdate: Faker::Date.backward,
       phone1: ('+996' + phonecodes.sample.to_s + rand(100000..999999).to_s),
       phone2: ('0' + phonecodes.sample.to_s + rand(100000..999999).to_s),
       skype: ((Faker::Name.name.downcase!).split(' ')).join('_'),
       passportdetails: Faker::Lorem.word,
-      email: Faker::Internet.email,
+      email: Faker::Internet.safe_email,
       password: password,
       password_confirmation: password
   )
 end
 
 manager = User.create!(name: 'manager',
-                       surname: 'shit',
-                       gender: 'male',
+                       surname: 'manager',
+                       gender: 'Мужчина',
                        birthdate: '02.09.1992',
                        phone1: '+996772180825',
                        skype: 'skype.daniyar',
@@ -43,3 +43,36 @@ manager = User.create!(name: 'manager',
                        email: 'manager@test.com', password: password, password_confirmation: password)
 
 manager.add_role 'manager'
+
+student = User.create!(name: 'Student',
+                       surname: 'Lastname',
+                       gender: 'Мужчина',
+                       birthdate: '02.09.1992',
+                       phone1: '+996772180825',
+                       skype: 'skype.daniyar',
+                       passportdetails:'abijjljlk',
+                       email: 'student@test.com', password: password, password_confirmation: password)
+
+student.add_role 'student'
+
+admin = User.create!(name: 'Admin',
+                       surname: 'Lastname',
+                       gender: 'Мужчина',
+                       birthdate: '02.09.1992',
+                       phone1: '+996772180825',
+                       skype: 'skype.admin',
+                       passportdetails:'MVD 50-01',
+                       email: 'admin@test.com', password: password, password_confirmation: password)
+
+admin.add_role 'admin'
+
+tutor = User.create!(name: 'Tutor',
+                       surname: 'Lastname',
+                       gender: 'Мужчина',
+                       birthdate: '02.09.1992',
+                       phone1: '+996772180825',
+                       skype: 'skype.tutor',
+                       passportdetails:'MVD 50-01',
+                       email: 'tutor@test.com', password: password, password_confirmation: password)
+
+tutor.add_role 'tutor'
