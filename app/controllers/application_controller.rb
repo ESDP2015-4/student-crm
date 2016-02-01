91ff372c416cb7caf535a2797e3c8a74745ab70f
daniyar <shamkeyev@mail.ru>
2016-02-01 23:49:01 +0600

    Included ApplicationHelper in ApplicationController to use in controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  include ApplicationHelper
+
   rescue_from CanCan::AccessDenied do |exception|
     respond_to do |format|
       format.json { render nothing: true, status: :forbidden }
