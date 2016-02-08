8e7d8b0851f9f08aec53ca51bef99d5136d1a3c2
shamkeev <shamkeyev@mail.ru>
2016-02-08 22:34:17 +0600

    Added abilities

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -9,7 +9,7 @@ class Ability
     if user.is_admin?
       #Может управлять всем
       can :manage, :all
-      can :assign_roles, User
+
     elsif user.is_manager?
       #Может управлять всем, пока что
       can :manage, Course
@@ -19,12 +19,24 @@ class Ability
       can :manage, GroupMembership
       can :manage, Course
       can :manage, User
+      can :manage, StudyUnit
 
     elsif user.is_teacher?
       #Может просматривать профили всех пользователей
       can :read, User
       #Может управлять своим профилем
       can [:show, :update], User, :id => user.id
+      # Может управлять занятиями --- необходимо добавить только свои занятия!!!!!!!!!!!
+      can :manage, Period
+
+      can :manage, Course, :id => user.teacher_courses.ids
+
+      # Может управлять раздатками только своих курсов
+      can [:manage], CourseElement, :id => CourseElement.where(course_id: user.teacher_courses.ids).ids
+
+      can :read, Group
+
+      can :manage, Attendance
 
     elsif user.is_techsupport?
       #Может просматривать профили всех пользователей
@@ -36,6 +48,16 @@ class Ability
       #Может управлять своим профилем
       can [:show, :update ], User, :id => user.id
       can :manage, Homework
+
+      # Может просматривать только свои курсы
+      can [:read], Course, :id => user.student_courses.ids
+
+      # Может просматривать раздатки только своих курсов
+      can [:read], CourseElement, :id => CourseElement.where(course_id: user.student_courses.ids).ids
+
+      can [:read], Period
+
+      can [:create, :update, :show, :index, :replace], Homework, :id => user.id
     else
 
     end
