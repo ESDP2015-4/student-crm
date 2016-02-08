fea35fe755a527428b26cd99552865fd7779a468
shamkeev <shamkeyev@mail.ru>
2016-02-09 00:07:56 +0600

    Changed redirect page upon access denial

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::Base
   rescue_from CanCan::AccessDenied do |exception|
     respond_to do |format|
       format.json { render nothing: true, status: :forbidden }
-      format.html { redirect_to main_app.root_url, :alert => exception.message }
+      format.html { redirect_to :back, :alert => exception.message }
     end
   end
 
