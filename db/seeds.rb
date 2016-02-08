afd87937513b1cb5258dc5735e9c5eccaaac05ae
shamkeev <shamkeyev@mail.ru>
2016-02-08 22:06:53 +0600

    Added comments

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,26 +1,28 @@
 
+# Create roles
 Role.create!(id: 1, name: 'student')
 Role.create!(id: 2, name: 'manager')
 Role.create!(id: 3, name: 'teacher')
 Role.create!(id: 4, name: 'techsupport')
 Role.create!(id: 5, name: 'admin')
 
+# Create study units
 StudyUnit.create!(title:'Первый')
 StudyUnit.create!(title:'Второй')
 StudyUnit.create!(title:'Третий')
 StudyUnit.create!(title:'Четвертый')
 StudyUnit.create!(title:'Пятый')
 
-
+# Create phone codes
 phonecodes = (550..559).to_a
 phonecodes.push (700..709).to_a
 phonecodes.push (770..779).to_a
-# phonecodes.push [543, 545] #I don't know if anyone is still using fonex
 phonecodes.flatten!
 
+# Default password for test users
 password = 'password'
 
-#creating students
+# Creating students
 50.times do
   User.create!(
       name: Faker::Name.first_name,
@@ -39,6 +41,7 @@ password = 'password'
   )
 end
 
+# Test user with manager role
 manager = User.create!(name: 'manager',
                        surname: 'manager',
                        gender: 'Мужчина',
@@ -51,6 +54,7 @@ manager = User.create!(name: 'manager',
 
 manager.add_role 'manager'
 
+# Test user with a student role
 student = User.create!(name: 'Student',
                        surname: 'Lastname',
                        gender: 'Мужчина',
@@ -63,6 +67,7 @@ student = User.create!(name: 'Student',
 
 student.add_role 'student'
 
+# Test user with an admin role
 admin = User.create!(name: 'Admin',
                      surname: 'Lastname',
                      gender: 'Мужчина',
@@ -75,6 +80,7 @@ admin = User.create!(name: 'Admin',
 
 admin.add_role 'admin'
 
+# Test user with a teacher role
 teacher = User.create!(name: 'Teacher',
                      surname: 'Lastname',
                      gender: 'Мужчина',
@@ -87,6 +93,7 @@ teacher = User.create!(name: 'Teacher',
 
 teacher.add_role 'teacher'
 
+# Test user with a techsupport role
 support = User.create!(name: 'Tech',
                      surname: 'Support',
                      gender: 'Мужчина',
@@ -99,14 +106,14 @@ support = User.create!(name: 'Tech',
 
 support.add_role 'techsupport'
 
-element_types = ['Лекция', 'Вебинар', 'Лабараторка', 'Контрольная']
+element_types = ['Лекция', 'Вебинар', 'Лабараторка', 'Контрольная']  # Types of course elements
 
-student_id = 0
+student_id = 0 # used to add created students to groups by id
 5.times do
   course = Course.create!(name: "#{Faker::Hacker.adjective.capitalize} #{Faker::Hacker.noun.capitalize}")
 
   10.times do
-    ce = CourseElement.create!(
+    CourseElement.create!(
         theme:"#{Faker::Hacker.ingverb.capitalize} #{Faker::Hacker.abbreviation.capitalize}",
         course: course,
         element_type: element_types.sample,
@@ -114,7 +121,7 @@ student_id = 0
     )
   end
 
-  gr_num = 0
+  gr_num = 0 # Used for a group title
   2.times do
     gr_num += 1
     group = Group.create!(
@@ -123,7 +130,7 @@ student_id = 0
         course: course
     )
 
-    #create GroupMembership for this group
+    # Add 5 students to a group
     5.times do
       #increment student_id to get different student ids
       student_id += 1
@@ -132,17 +139,21 @@ student_id = 0
           user_id: student_id,
           active: true)
     end
-
+    # Add teacher to a group
     TeachersGroup.create!(group: group,
           user_id: teacher.id,
           active: true)
-
+    # Add support to a group
     SupportsGroup.create!(group: group,
           user_id: support.id,
           active: true)
 
   end
 end
+
+# Add Test Student to groups (Course No. 1 and Course No. 2)
+GroupMembership.create!(group_id: 1, user_id: 52, active: true)
+GroupMembership.create!(group_id: 3, user_id: 52, active: true)
 
 ######################### start of period seeds ###############
 
