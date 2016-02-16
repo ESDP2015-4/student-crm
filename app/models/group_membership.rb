a2b795b3023bf6ea53e767fc9a378f3516c361e2
shamkeev <shamkeyev@mail.ru>
2016-02-16 20:13:10 +0600

    Added method to create attendances when a new student is added to an existing group

--- app/models/group_membership.rb
+++ app/models/group_membership.rb
@@ -1,4 +1,12 @@
 class GroupMembership < ActiveRecord::Base
   belongs_to :group
   belongs_to :student, class_name: "User", foreign_key: 'user_id'
+
+  after_save :create_attendances
+  # Will create attendances if a user is added to a group
+  def create_attendances
+    self.group.periods.each do |period|
+      Attendance.create!(period: period, user: self.student)
+    end
+  end
 end
