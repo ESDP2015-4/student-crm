d49dbad1508e7fd3a7a2c0b6ec33f98587ab2601
altynbek sayak <altiwbek@gmail.com>
2016-02-16 22:04:01 +0600

    omni

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, "",
-           "", {
+  provider :google_oauth2, "907497187323-4svde7p9q09s8tqvo6qj14c02jtr03r6.apps.googleusercontent.com",
+           "yzjuDIehwLebdZYEETQj7Y4h", {
                scope: ['email',
                        'profile',
                        'https://www.googleapis.com/auth/gmail.modify',
