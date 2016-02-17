30382b4705fa69c234e01bc26d4621bda9b4a44c
Amirkhan Mullabaev <a.mullabaev@gmail.com>
2016-02-17 18:07:08 +0600

    Переписал тесты для курсов

--- features/step_definitions/course.rb
+++ features/step_definitions/course.rb
@@ -1,8 +1,1 @@
-#xpath that finds links in sidebar by name
-# .//*[@id='sidebar-wrapper']/ul/li/a/span[contains(.,'Группы')]
-
-When(/^я кликаю на "([^"]*)" возле названия курса Test course$/) do |edit|
-  within(:xpath, "//tbody/tr[td/a='Test course']") do
-    click_on(edit)
-  end
-end
+#all steps are common with other scenarios, so i put them to to ./common.rb
